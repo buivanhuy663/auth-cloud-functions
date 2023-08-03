145b05da05c6793170cf2b00897783d12c6708a5
buivanhuy663 <buivanhuy.663@gmail.com>
2023-08-03 20:30:24 +0700

    add function

---
+++ functions/src/use_case/auth_use_case/config_use_case.ts
@@ -0,0 +1,13 @@
+import {DocumentData} from "firebase-admin/firestore"
+import {PromiseOutputUseCase} from "../../base/use_case/promise_output_use_case"
+import {firestoreHelper} from "../../helper/firestore_helper"
+import {ConstantKey} from "../../utilities/constant_key"
+
+class ConfigUseCase implements PromiseOutputUseCase<DocumentData | undefined> {
+    async run(): Promise<DocumentData | undefined> {
+        const data = await firestoreHelper.getDocument(ConstantKey.congig, ConstantKey.docEmail)
+        return data.data()
+    }
+}
+
+export const configUseCase = new ConfigUseCase()
---
+++ functions/src/utilities/constant_key.ts
@@ -0,0 +1,8 @@
+export class ConstantKey {
+    static readonly accountAuthCode = 'accounts-authcode'
+    static readonly congig = 'config'
+    static readonly docEmail = 'auth-email'
+    static readonly timestamp = 'timestamp'
+    static readonly accountsInfos = 'accounts-info'
+    static readonly accountsAuth = 'accounts-auth'
+}
